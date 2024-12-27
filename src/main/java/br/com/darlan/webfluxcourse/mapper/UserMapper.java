44f6501f1d597dc7f0004e6c27abbb9d2f302cc2
gabriel.darlan88@gmail.com <gabriel.darlan88@gmail.com>
2024-12-26 21:03:59 -0300

    validando a criacao de usuario, adicionando a validacao valores nulos

--- src/main/java/br/com/darlan/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/darlan/webfluxcourse/mapper/UserMapper.java
@@ -5,8 +5,13 @@ import br.com.darlan.webfluxcourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = IGNORE,
+        nullValueCheckStrategy = ALWAYS
 )
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
