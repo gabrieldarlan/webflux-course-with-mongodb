c67be986c80715ca0ec558915f64609301e05aef
gabriel.darlan88@gmail.com <gabriel.darlan88@gmail.com>
2025-01-04 12:48:02 -0300

    adicionando o TrimString annotation

--- src/main/java/br/com/darlan/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/darlan/webfluxcourse/model/request/UserRequest.java
@@ -1,17 +1,20 @@
 package br.com.darlan.webfluxcourse.model.request;
 
+import br.com.darlan.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         String name,
         @NotBlank(message = "must not be null or empty")
-
+        @TrimString
         @Email(message = "invalid e-mail")
         String email,
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         String password
---
+++ src/main/java/br/com/darlan/webfluxcourse/validator/TrimString.java
@@ -0,0 +1,22 @@
+package br.com.darlan.webfluxcourse.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Constraint(validatedBy = {TrimStringValidator.class})
+@Target(FIELD)
+@Retention(RUNTIME)
+public @interface TrimString {
+
+    String message() default "field cannot contain spaces at the beginning or end";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
