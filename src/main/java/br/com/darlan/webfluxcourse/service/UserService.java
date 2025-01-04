a48353b69d14920aef31347ed6416b9fb7b34806
gabriel.darlan88@gmail.com <gabriel.darlan88@gmail.com>
2025-01-04 13:49:18 -0300

    adicionando find all

--- src/main/java/br/com/darlan/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/darlan/webfluxcourse/service/UserService.java
@@ -7,6 +7,7 @@ import br.com.darlan.webfluxcourse.repository.UserRepository;
 import br.com.darlan.webfluxcourse.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
@@ -27,5 +28,9 @@ public class UserService {
                 .switchIfEmpty(Mono.error(
                         new ObjectNotFoundException(format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName()))
                 ));
+    }
+
+    public Flux<User> findAll() {
+        return repository.findAll();
     }
 }
